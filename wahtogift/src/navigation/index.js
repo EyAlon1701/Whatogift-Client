a9936efd274582fb30c007041d625f76846b08d5
EyAlon1701 <eyalon022@gmail.com>
2022-11-07 13:52:20 +0200

    Update index.js

--- wahtogift/src/navigation/index.js
+++ wahtogift/src/navigation/index.js
@@ -7,16 +7,28 @@ import MaterialCommuniyIcons from 'react-native-vector-icons/MaterialCommunityIc
 
 //IMPORT SCREENS
 import Dashboard from '../screens/dashboard';
+import Test from '../screens/dashboard/test'
 import Favorites from '../screens/favorites';
 import Gifts from '../screens/gifts';
 import Profile from '../screens/profile';
+
+//CREATE STACK
+const dashboardStackNavigator = createNativeStackNavigator();
+export const DashboardStack = () => {
+    return(
+        <dashboardStackNavigator.Navigator>
+            <dashboardStackNavigator.Screen name='dashboard' component={Dashboard} />
+            <dashboardStackNavigator.Screen name='test' component={Test} />
+        </dashboardStackNavigator.Navigator>
+    )
+}
 
 //CREATE TABS
 const AppTab = createMaterialBottomTabNavigator();
 export const TabsNavigator = () => {
     return(
         <AppTab.Navigator>
-            <AppTab.Screen name='dashboardTab' component={Dashboard}
+            <AppTab.Screen name='dashboardTab' component={DashboardStack}
                 options={{ tabBarLabel: 'Dashboard', tabBarIcon: () => (<MaterialCommuniyIcons name='view-dashboard' size={28} />)}} />  
             <AppTab.Screen name='favoritesTab' component={Favorites}
                 options={{ tabBarLabel: 'Favorites', tabBarIcon: () => (<MaterialCommuniyIcons name='view-grid' size={28} />)}} />  
